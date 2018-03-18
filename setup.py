f3d144298a46aa9a2a51da8e3b01b9893d6587ce
Peter Boin <Peter.Boin@gmail.com>
2018-03-18 14:55:53 +1100

    explicit readme encoding to utf-8

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     author='David Cowden',
     author_email='dave.cowden@gmail.com',
     description='CadQuery is a parametric  scripting language for creating and traversing CAD models',
-    long_description=open('README.md').read(),
+    long_description=open('README.md', encoding='UTF-8').read(),
     packages=['cadquery','cadquery.contrib','cadquery.freecad_impl','cadquery.plugins','tests'],
     install_requires=['pyparsing'],
     include_package_data=True,
